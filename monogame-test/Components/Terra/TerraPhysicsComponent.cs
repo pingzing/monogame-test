8940f5b8fe6c7b97a9375f76b031581351353561
Neil McAlister <neil.mcalister@futurice.com>
2017-06-15 16:20:22 +0300

    Reverse order of collision detection, Y-axis first

--- monogame-test/Components/Terra/TerraPhysicsComponent.cs
+++ monogame-test/Components/Terra/TerraPhysicsComponent.cs
@@ -51,73 +51,46 @@ namespace monogame_test.Components.Terra
                 (DefaultBoundingBoxSize.X * entity.Scale),
                 (DefaultBoundingBoxSize.Y * entity.Scale));
 
-            // Check X-axis collision
-            RectangleF correctedBoundingBox = entity.GetOriginCorrectedBoundingBox(proposedNewBoundingBox);
-            bool collidedWith = map.IsColliding(correctedBoundingBox);
-            if (collidedWith)
+            for (int i = 0; i < 3; i++)
             {
-                float xCorrection = 0f;        
+                // Next, check Y-axis collision
+                RectangleF correctedBoundingBox = entity.GetOriginCorrectedBoundingBox(proposedNewBoundingBox);
+                bool verticalCollidedWith = map.IsColliding(correctedBoundingBox);
+                if (verticalCollidedWith)
+                {
+                    float yCorrection = GetYCorrection(proposedNewPosition, correctedBoundingBox, map, entity);
+
+                    proposedNewPosition = new Vector2(proposedNewPosition.X, proposedNewPosition.Y + yCorrection);
+                    proposedNewBoundingBox = new RectangleF
+                        (proposedNewPosition.X,
+                        proposedNewPosition.Y,
+                        (DefaultBoundingBoxSize.X * entity.Scale),
+                        (DefaultBoundingBoxSize.Y * entity.Scale));
+
+                    entity.Velocity = new Vector2(entity.Velocity.X, 0);
+                }
+
+                // Check X-axis collision
+                correctedBoundingBox = entity.GetOriginCorrectedBoundingBox(proposedNewBoundingBox);
+                bool horizontalCollidedWith = map.IsColliding(correctedBoundingBox);
+                if (horizontalCollidedWith)
+                {
+                    float xCorrection = GetXCorrection(proposedNewPosition, correctedBoundingBox, map, entity);                    
+
+                    proposedNewPosition = new Vector2(proposedNewPosition.X + xCorrection, proposedNewPosition.Y);
+                    proposedNewBoundingBox = new RectangleF
+                        (proposedNewPosition.X,
+                        proposedNewPosition.Y,
+                        (DefaultBoundingBoxSize.X * entity.Scale),
+                        (DefaultBoundingBoxSize.Y * entity.Scale));
+
+                    entity.Velocity = new Vector2(0, entity.Velocity.Y);
+                }
                 
-                // Going right
-                if (proposedNewPosition.X > entity.Position.X)
+                if (!horizontalCollidedWith && !verticalCollidedWith)
                 {
-                    float bboxXCoordinate = correctedBoundingBox.Right;
-                    float bboxMiddleY = correctedBoundingBox.Center.Y;
-                    RectangleF obstacleBbox = map.GetNearestHorizontalCollidedObject(bboxXCoordinate, bboxMiddleY, true);
-                    xCorrection = (Math.Abs(bboxXCoordinate - obstacleBbox.Left) + 1) * -1;                    
-
+                    break;
                 }
-                //going left
-                else if (proposedNewPosition.X < entity.Position.X)
-                {
-                    float bboxXCoordinate = correctedBoundingBox.Left;
-                    float bboxMiddleY = correctedBoundingBox.Center.Y;
-                    RectangleF obstacleBbox = map.GetNearestHorizontalCollidedObject(bboxXCoordinate, bboxMiddleY, false);
-                    xCorrection = Math.Abs(bboxXCoordinate - obstacleBbox.Right) + 1;
-                }
-
-                proposedNewPosition = new Vector2(proposedNewPosition.X + xCorrection, proposedNewPosition.Y);
-                proposedNewBoundingBox = new RectangleF
-                    (proposedNewPosition.X,
-                    proposedNewPosition.Y,
-                    (DefaultBoundingBoxSize.X * entity.Scale),
-                    (DefaultBoundingBoxSize.Y * entity.Scale));
-
-                entity.Velocity = new Vector2(0, entity.Velocity.Y);
-            }
-
-            // Next, check Y-axis collision
-            correctedBoundingBox = entity.GetOriginCorrectedBoundingBox(proposedNewBoundingBox);
-            collidedWith = map.IsColliding(correctedBoundingBox);
-            if (collidedWith)
-            {                
-                float yCorrection = 0f;
-
-                // Going down
-                if (proposedNewPosition.Y > entity.Position.Y)
-                {
-                    float bboxYCoordinate = correctedBoundingBox.Bottom;
-                    float bboxMiddleX = correctedBoundingBox.Center.X;
-                    RectangleF obstacleBox = map.GetNearestVerticalCollidedObject(bboxYCoordinate, bboxMiddleX, true);
-                    yCorrection = (Math.Abs(bboxYCoordinate - obstacleBox.Top) + 1) * -1;
-                }
-                // Going up
-                else if (proposedNewPosition.Y < entity.Position.Y)
-                {
-                    float bboxYCoordinate = correctedBoundingBox.Top;
-                    float bboxMiddleX = correctedBoundingBox.Center.X;
-                    RectangleF obstacleBox = map.GetNearestVerticalCollidedObject(bboxYCoordinate, bboxMiddleX, false);
-                    yCorrection = Math.Abs(bboxYCoordinate - obstacleBox.Bottom) + 1;
-                }                
-
-                proposedNewPosition = new Vector2(proposedNewPosition.X, proposedNewPosition.Y + yCorrection);
-                proposedNewBoundingBox = new RectangleF
-                    (proposedNewPosition.X,
-                    proposedNewPosition.Y,
-                    (DefaultBoundingBoxSize.X * entity.Scale),
-                    (DefaultBoundingBoxSize.Y * entity.Scale));
-
-                entity.Velocity = new Vector2(entity.Velocity.X, 0);
             }
                            
             entity.Position = proposedNewPosition;
@@ -126,6 +99,55 @@ namespace monogame_test.Components.Terra
             {
                 System.Diagnostics.Debug.WriteLine($"POS: X: {entity.Position.X}\t Y: {entity.Position.Y}\t BBOX: X: {entity.BoundingBox.X} Y: {entity.BoundingBox.Y} ");
             }
+        }
+
+        private float GetXCorrection(Vector2 proposedNewPosition, RectangleF correctedBoundingBox, TestMap map, Entity entity)
+        {
+            float xCorrection = 0f;
+
+            // Going right
+            if (proposedNewPosition.X > entity.Position.X)
+            {
+                float bboxXCoordinate = correctedBoundingBox.Right;
+                float bboxMiddleY = correctedBoundingBox.Center.Y;
+                RectangleF obstacleBbox = map.GetNearestHorizontalCollidedObject(bboxXCoordinate, bboxMiddleY, true);
+                xCorrection = (Math.Abs(bboxXCoordinate - obstacleBbox.Left) + 1) * -1;
+
+            }
+            //going left
+            else if (proposedNewPosition.X < entity.Position.X)
+            {
+                float bboxXCoordinate = correctedBoundingBox.Left;
+                float bboxMiddleY = correctedBoundingBox.Center.Y;
+                RectangleF obstacleBbox = map.GetNearestHorizontalCollidedObject(bboxXCoordinate, bboxMiddleY, false);
+                xCorrection = Math.Abs(bboxXCoordinate - obstacleBbox.Right) + 1;
+            }
+
+            return xCorrection;
+        }
+
+        private float GetYCorrection(Vector2 proposedNewPosition, RectangleF correctedBoundingBox, TestMap map, Entity entity)
+        {
+            float yCorrection = 0f;
+
+            // Going down
+            if (proposedNewPosition.Y > entity.Position.Y)
+            {
+                float bboxYCoordinate = correctedBoundingBox.Bottom;
+                float bboxMiddleX = correctedBoundingBox.Center.X;
+                RectangleF obstacleBox = map.GetNearestVerticalCollidedObject(bboxYCoordinate, bboxMiddleX, true);
+                yCorrection = (Math.Abs(bboxYCoordinate - obstacleBox.Top) + 1) * -1;
+            }
+            // Going up
+            else if (proposedNewPosition.Y < entity.Position.Y)
+            {
+                float bboxYCoordinate = correctedBoundingBox.Top;
+                float bboxMiddleX = correctedBoundingBox.Center.X;
+                RectangleF obstacleBox = map.GetNearestVerticalCollidedObject(bboxYCoordinate, bboxMiddleX, false);
+                yCorrection = Math.Abs(bboxYCoordinate - obstacleBox.Bottom) + 1;
+            }
+
+            return yCorrection;
         }
     }
 }
